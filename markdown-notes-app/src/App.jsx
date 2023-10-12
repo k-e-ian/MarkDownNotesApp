5e5b15ba82644322befaabc0c5c256bb4baf8842
ian kitembe <ianedwin@outlook.com>
2023-10-12 08:57:06 +0300

    Sync notes with localStorage using lazy initialization

--- markdown-notes-app/src/App.jsx
+++ markdown-notes-app/src/App.jsx
@@ -7,11 +7,19 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 import './App.css';
 
-export default function App() {
-    const [notes, setNotes] = React.useState([])
+export default function App() {    
+    const [notes, setNotes] = React.useState(
+      () => JSON.parse(localStorage.getItem("notes")) || [])
+
+    React.useEffect(() => {
+      localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    console.log(notes)
 
     function createNewNote() {
       const newNote = {
